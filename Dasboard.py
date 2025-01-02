bd5c7cce2aab7a7691543cfc17242d9b1b733afe
Sondos Wael <sondos.wael2025@gmail.com>
2025-01-02 19:35:21 +0200

    updates in dashboard

--- Dasboard.py
+++ Dasboard.py
@@ -16,7 +16,9 @@ clean_complete = pd.read_csv("covid_19_clean_complete_updated.csv")
 full_grouped = pd.read_csv("full_grouped_updated.csv")
 
 diff_cases = ['Total Cases', 'Total Deaths', 'Total Recovered', 'Active Cases', 'Serious/Critical','Total Tests']
-
+usa_cases = ['Confirmed', 'Deaths']
+usa_pie_fig = px.pie(usa, values=usa[usa_cases].sum(), names=usa_cases,
+             title='Different cases of Covid 19 in USA')
 # different cases for Covid 19 in World data
 f_diff_world = px.pie(world, values=world[diff_cases].sum(), names=diff_cases,
              title='Different cases of Covid 19')
@@ -34,14 +36,20 @@ app.layout = dmc.Container([
         ])
 
     ]),
-dmc.Grid([
+    dmc.Grid([
     dmc.Col([
         dcc.Graph(figure=f_diff_world, id='Different_cases')
     ], span = 6),
 
         dmc.Col([
-                dcc.Graph(figure ={},id='Different_Cases_mean_in_Continent')
+                dcc.Graph(figure ={}, id='Different_Cases_mean_in_Continent')
         ],span = 6)
+    ]),
+    dmc.Grid([
+        dmc.Col([
+            dcc.Graph(figure = usa_pie_fig, id = "pie_usa")
+        ],span = 6)
+
     ])
 ], fluid = True
 )
@@ -50,7 +58,7 @@ dmc.Grid([
     Output(component_id='Different_Cases_mean_in_Continent', component_property='figure'),
     Input(component_id='Different cases', component_property='value')
 )
-def update_graph(col_chosen):
+def update_world_plots(col_chosen):
 
     fig = px.choropleth(world, locations='Country/Region', locationmode='country names',
                         color=col_chosen, hover_name='Continent', projection='natural earth',
@@ -59,8 +67,8 @@ def update_graph(col_chosen):
     gro_data_cont =world.groupby('Continent')[col_chosen].mean().reset_index()
     f_cont_world = px.bar(gro_data_cont, x='Continent',
                           y=col_chosen,color_discrete_sequence=['#5DE2E7'])
-
     return fig,f_cont_world
+
 
 
 if __name__ == '__main__':
